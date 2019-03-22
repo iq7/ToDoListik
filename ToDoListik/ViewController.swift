8cbb61462ad07c65c6abc9ffe40242eb1d497951
iq7 <iq_se7en@mail.ru>
2019-03-22 22:11:25 +0300

    Initial Commit

---
+++ ToDoListik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListik
+//
+//  Created by Андрей Тихонов on 22/03/2019.
+//  Copyright © 2019 Андрей Тихонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
